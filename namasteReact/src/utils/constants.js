628e6e6b48a5d1f38b6d21a56716555b9bace412
ParshuShah <learnerparshuram@gmail.com>
2024-12-30 20:00:50 +0530

    Add files via upload

---
+++ namasteReact/src/utils/constants.js
@@ -0,0 +1,6 @@
+
+
+export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+
+export const LOGO_URL = "https://img.freepik.com/premium-vector/logo-food-company-that-says-sun-sun-sunflower_917213-253424.jpg?w=740";
