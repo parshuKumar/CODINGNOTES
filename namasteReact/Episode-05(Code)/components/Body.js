b1cd996d666b4f8e8703937edb6a7991baf958df
ParshuShah <learnerparshuram@gmail.com>
2024-12-30 20:03:25 +0530

    Add files via upload

---
+++ namasteReact/Episode-05(Code)/components/Body.js
@@ -0,0 +1,56 @@
+import RestaurantCard from "./RestaurantCard";
+import resList from "../utils/mockData"
+import {useState} from "react";
+
+const Body = () => {
+
+    //Local State variable - Super Powerful variable
+    const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+
+    return (
+        <div className = "body">
+            <div className = "filter">
+                <button className = "filter-btn"
+
+                    onClick={() => {
+                        const filteredList = listOfRestaurants.filter(
+                            (res) => (res.info.avgRatingString) > 4
+                        );
+                   
+                setListOfRestaurants(filteredList);
+                console.log("Button clicked")
+            } } 
+                >Top Rated Restaurants </button>
+            </div>
+            <div className = "res-container" >
+                {/*
+                 <RestaurantCard resData = {resList[0]} />
+                <RestaurantCard resData = {resList[1]} />
+                <RestaurantCard resData = {resList[2]} />
+                <RestaurantCard resData = {resList[3]} />
+                <RestaurantCard resData = {resList[4]} />
+                <RestaurantCard resData = {resList[5]} /> 
+                we will make this modular too by using the for loop
+                or we can also do so map filer or reduce;
+                
+                */}
+                {
+                    listOfRestaurants.map((restaurant) => (
+                    <RestaurantCard key = {restaurant.info.id} resData = {restaurant}/>
+                    ))
+
+                    // resList.map((restaurant , index) => (
+                    //     <RestaurantCard key = {index} resData = {restaurant}/>
+                    //     ))
+                    //we can use index but we should not
+                    //even react tell us that we should not use the index as a key
+                    //and always put the key in this rather than not having the key        
+
+
+                }
+            </div>
+        </div>
+    )
+}
+
+export default Body;
---
+++ namasteReact/Episode-05(Code)/components/RestaurantCard.js
@@ -0,0 +1,44 @@
+import {CDN_URL} from "../utils/constants"
+
+const RestaurantCard = (props) => {
+    const{resData} = props; //destructuring the props (props is react) and destructuring is js
+    
+    const {
+     cuisines, 
+     avgRatingString,
+     costForTwo,
+     slaParshu = {}} = resData?.info;
+    
+    
+    return (
+         <div className = "res-card" style = {{backgroundColor : "#f0f0f0"}}> 
+            <img 
+            className = "res-logo"
+            alt="res-logo"
+            src = {CDN_URL + resData.info.cloudinaryImageId}
+            />
+            {/* 
+            <h3>{resData.info.name}</h3>
+            <h4>{resData.info.cuisines.join(", ")}</h4>
+            <h4>{resData.info.avgRatingString} stars</h4>
+            <h4>{resData.info.costForTwo}</h4>
+            <h4>{resData.info.sla.deliveryTime} minutes</h4> 
+            This is not a good practice to write the code
+            so we have destructuree the code above and now you
+            can see the below example; 
+            */}
+            
+           
+ 
+            <h3>{name}</h3>
+            <h4>{cuisines.join(", ")}</h4>
+            <h4>{avgRatingString} stars</h4>
+            <h4>{costForTwo}</h4>
+            <h4>{slaParshu.deliveryTime} minutes</h4>
+ 
+ 
+         </div>
+     )
+ }
+
+ export default RestaurantCard;
