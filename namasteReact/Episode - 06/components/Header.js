5eafe1a668df72cbb8908c6dac407aa396410c11
ParshuShah <learnerparshuram@gmail.com>
2024-12-31 15:28:33 +0530

    Add files via upload

---
+++ namasteReact/Episode - 06/components/Header.js
@@ -0,0 +1,37 @@
+import {LOGO_URL} from "../utils/constants";
+import {useState} from "react";
+
+
+const Header = () => {
+
+    // let btnName = "Login"; //we are trying with HTML
+
+    //useState variable
+    const [btnNameReact, setBtnNameReact] = useState("Login");
+
+    return (
+        <div className = "header">
+            <div className = "logo-container">
+                <img className = "logo" src = {LOGO_URL} />
+            </div>
+        
+        <div className = "nav-items">
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact Us</li>
+                <li>Cart</li>
+                {/* <button className = "login" 
+                onClick={()=>{btnName = "Logout"}}>{btnName}</button> */}
+                <button className = "login" 
+                onClick={()=>{
+                    btnNameReact === "Login" ? setBtnNameReact("Logout") : setBtnNameReact("Login");
+                }}>{btnNameReact}</button> 
+            </ul>
+        </div>
+        </div>
+    )
+}
+
+
+export default Header;
